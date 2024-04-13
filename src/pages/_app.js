1360c3e7b6201a86801390b7c6c810b19346a7b3
Paul <paultham4@gmail.com>
2024-04-13 16:12:33 +0800

    add redux

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,14 @@
 import { SessionProvider } from "next-auth/react"
+import store from '@/stores/store'
+import { Provider } from 'react-redux'
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
+        <Provider store={store}>
           <Component {...pageProps} />
+        </Provider>
     </SessionProvider>
 )
 }
---
+++ src/stores/currentPageSlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export const currentPageSlice = createSlice({
+  name: 'currentPage',
+  initialState: {
+    value: 0,
+  },
+  reducers: {
+    setPage: (state, action) => {
+      // Redux Toolkit allows us to write "mutating" logic in reducers. It
+      // doesn't actually mutate the state because it uses the Immer library,
+      // which detects changes to a "draft state" and produces a brand new
+      // immutable state based off those changes.
+      // Also, no return statement is required from these functions.
+      state.value = action.payload
+    },
+  },
+})
+
+export const PAGE_ID = {
+  DASHBOARD: 0,
+  DOCUMENTS: 1,
+  GOOGLE_DRIVE: 2,
+  MSONE_DRIVE: 3,
+}
+
+// Action creators are generated for each case reducer function
+export const { setPage } = currentPageSlice.actions
+
+export default currentPageSlice.reducer
